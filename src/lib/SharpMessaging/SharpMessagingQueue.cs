e7a59088b0f262e429ccec13f4453652181c5903
jgauffin <jonas.gauffin@gmail.com>
2015-02-16 20:52:18 +0100

    started to sketch on the servicebroker.

---
+++ src/lib/SharpMessaging/SharpMessagingQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpMessaging.Frames;
+using SharpMessaging.Server;
+
+namespace SharpMessaging
+{
+    public class SharpMessagingQueue
+    {
+
+    }
+
+    public interface IMessagingService
+    {
+        
+    }
+    public class SharpMessagingServiceBroker
+    {
+        private SharpMessagingServer _server;
+        ConcurrentDictionary<string, IMessagingService> _items = new ConcurrentDictionary<string, IMessagingService>();
+        public SharpMessagingServiceBroker()
+        {
+            _server = new SharpMessagingServer();
+            _server.FrameReceived += OnFrame;
+            
+        }
+
+        public bool AllowDynamicQueues { get; set; }
+        public bool AllowDynamicTopics { get; set; }
+
+        private void OnFrame(ServerClient arg1, MessageFrame arg2)
+        {
+            var destination = arg2.Destination;
+            IMessagingService service;
+            if (!_items.TryGetValue(destination, out service))
+            {
+                if (destination.StartsWith("topic/"))
+                {
+                    if (!AllowDynamicTopics)
+                    {
+                        var errorFrame = new ErrorFrame("Topics must exist before messages are sent. Cannot create " + destination);
+                        //arg1.Send(errorFrame);
+                    }
+                }
+        
+            }
+        }
+    }
+}
